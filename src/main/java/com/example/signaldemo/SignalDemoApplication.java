55b28444f380a7f7d497b3ac9ac4101ada2df080
jjunmo <yymeyy2002@gmail.com>
2024-04-02 11:17:53 +0900

    first commit

--- src/main/java/com/example/signaldemo/SignalDemoApplication.java
+++ src/main/java/com/example/signaldemo/SignalDemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SignalDemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SignalDemoApplication.class, args);
     }
-
 }
